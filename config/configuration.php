ee174e5edef1c7dda41cc97050b8b5c3981ade0a
ExusVX <exusvx@gmail.com>
2017-02-16 00:06:33 +0000

    V0.5

--- config/configuration.php
+++ config/configuration.php
@@ -1,20 +1,20 @@
 <?php
 /* 
-As of 25th Aug 2016, everything in this configuration is editable!
+As of 31th Jan 2017, everything in this configuration is editable!
 */
 /* The name you want to call your marketplace */
 $market_name = "DigiMarket";
 
-/* MySQL Information */
-$serverip = "localhost";
-$username = "exlabsco_dm";
-$password = "x275432";
-$dbname = "exlabsco_digimarket";
+/* MySQL Information (db = database) */
+$db_serverip = "localhost"; // Change if you aren't using localhost
+$db_username = "your db username";
+$db_password = "your db password";
+$db_name = "your db";
 
-$devmode = 1;
+$devmode = 1; // If you have this you probably want dev mode on (at 1), because its not even finished
 
 /* The folder that this is stored in, if its in the main directory where you land when you goto your domain, then just set this to your domain name! */
-$website_root_user = "http://wafflezzz.xyz/projects/ElectricMarket"; // Dont leave / at the end
+$website_root_user = "http://exlabs.net/DigiMarket-master"; // Dont leave / at the end - Yup thats my domain, I got it a while ago, going to be changing it soon.
 
 /* User timeout in minutes */
 $user_timeout = 30; 
@@ -39,4 +39,6 @@ $fileUploadPath = "/uploads/files";
 
 /* Self Explanatory -> Where all the images (excluding digital goods - see the above variable) are uploaded to */
 $imageUploadPath = "/uploads/images";
+
+$background = "39,40,34"; // RGB (I can't even remember if I got this working)
 ?>
